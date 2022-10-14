1f30a7a03dfbfa277cbb041092f5981793f7b181
Lewis <alewis38@cherrycreekschools.org>
2022-10-14 10:34:58 -0600

    tile pallets and maps suck ;-;

---
+++ challenge 2.4/Assets/aaaaahhh.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaaaahhh : MonoBehaviour
+{
+    int first = 10;
+    Rigidbody2D player;
+    int x = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = null;
+        Debug.Log(first);
+        Debug.Log(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
